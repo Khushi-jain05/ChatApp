0bc199afbcf3cb6f165d2d68f3075b686c0254de
Khushi-jain05 <kj5369227@gmail.com>
2026-01-13 18:58:56 +0530

    .

--- server/server.js
+++ server/server.js
@@ -44,8 +44,10 @@ app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
 await connectDB();
-
-const PORT = process.env.PORT || 5001;
-server.listen(PORT, () =>
-  console.log(`Server is running on port ${PORT}`)
-);
+if(process.env.NODE_ENV !== "production"){
+  const PORT = process.env.PORT || 5001;
+  server.listen(PORT, () =>
+    console.log(`Server is running on port ${PORT}`)
+  );
+}
+export default server;
